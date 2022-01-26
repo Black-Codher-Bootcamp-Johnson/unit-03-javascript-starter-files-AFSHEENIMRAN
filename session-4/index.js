715063e0e9b3280fa7acb436962f3b7168b44712
AFSHEEN <afsheenmunawar77@gmail.com>
2022-01-26 20:31:41 +0000

    java script conditional

--- session-4/index.js
+++ session-4/index.js
@@ -1,14 +1,55 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA = {
+    name: "Liah",
+    age: "22",
+    location: "manchester",
+    likes: "singing"
+}
+
+let personB = {
+    name: "john",
+    age: "23",
+    location: "barking",
+    likes: "dancing"
+}
+
+let personC = {
+    name: "Marry",
+    age: "24",
+    location: "leeds",
+    likes: "reading"
+}
+
+let personD = {
+    name: "nick",
+    age: "25",
+    location: "London",
+    likes: "football"
+}
+
+function biography(person) {
+return ""
+}
 
 
+console.log(biography(personA)
 
 
+); 
+
+console.log(biography(personB)
 
 
+);
+
+console.log(biography(personC)
+
+);
+
+console.log(biography(personD)
 
 
-
-
+);
 
 
 
@@ -19,24 +60,24 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x / y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y " + division); 
 
 
 
@@ -53,6 +94,16 @@
 
 
 // ****************************************************TASK 3****************************************************
+ 
+ const john= 34;
+
+const drivingAge= 18;
+
+const johnOldEnoughToDrive   = john>drivingAge 
+
+console.log(`am I old enough to drive?${johnOldEnoughToDrive}`)
+
+
 
 
 
